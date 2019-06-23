6a9c8642ce88be57655c62b4c310596dce17c548
herbert koelman <herbert.koelman@me.com>
2019-06-23 17:43:35 +0200

    refs #120 @1h (GH issue #17, pull request #20)
    - genrete a sonar cloud project properties file.
    - create a custom target for the build-wrapper program that comes with sonar.

--- cmake/SonarCloudConfig.cmake
+++ cmake/SonarCloudConfig.cmake
@@ -5,15 +5,17 @@ set(SONAR_CLOUD_HOME  https://sonarcloud.io  CACHE STRING "Sonar cloud home URL(
 set(SONAR_PROJECT_KEY        ${PROJECT_NAME} CACHE STRING "Sonar project key property (default is ${PROJECT_NAME})")
 set(SONAR_ORGANIZATION                       CACHE STRING "Organization (default is empty)")
 set(SONAR_ACCESS_TOKEN                       CACHE STRING "Access/login token (default is empty)")
-set(SONAR_WRAPPER_OUTPUT_DIR  bw-output      CACHE STRING "Sonar C/C++ wrapper output directory (default is empty)")
 
-set(SONAR_PROPERTIES_FILE                    CACHE STRING "If file exists, it will used as is (default is empty)")
+set(SONAR_WRAPPER_OUTPUT_DIR  ${CMAKE_CURRENT_BINARY_DIR}/bw-output  CACHE STRING "Sonar C/C++ wrapper output directory (default is bw-output)")
+
+set(SONAR_PROPERTIES_FILE    ${SONAR_PROPERTIES_FILE}  CACHE STRING "If file exists, it will used as is (default is empty)")
 
 if ( SONAR )
 
   find_program(SONAR_SCANNER sonar-scanner)
 
   if ( SONAR_SCANNER )
+
     if(NOT EXISTS ${SONAR_PROPERTIES_FILE} )
       message(STATUS "Generating SONAR properties file (${CMAKE_CURRENT_BINARY_DIR}/sonar-project.properties)")
       configure_file(${CMAKE_CURRENT_LIST_DIR}/sonar-project.properties.in sonar-project.properties)
@@ -28,6 +30,18 @@ if ( SONAR )
       COMMENT "run ${SONAR_SCANNER} -Dproject.settings=${SONAR_PROPERTIES_FILE} "
       )
     message(STATUS "Added custom target [code-quality]...")
+
+    find_program(SONAR_BUILD_WRAPPER build-wrapper-linux-x86-64)
+    if ( SONAR_BUILD_WRAPPER )
+      add_custom_target( build-wrapper
+        COMMAND ${SONAR_BUILD_WRAPPER} --out-dir ${SONAR_WRAPPER_OUTPUT_DIR} make clean all
+        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+        COMMENT "run SONAR's ${SONAR_BUILD_WRAPPER}"
+        )
+      message(STATUS "Added custom target [build-wrapper]...")
+      add_dependencies(code-quality build-wrapper)
+    endif()
+
 
   else()
     message(SEND_ERROR "Failed to find the program [sonar_scanner], make sure sonar tools are installed.")
